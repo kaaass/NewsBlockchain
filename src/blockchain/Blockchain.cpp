1da063fe85e2eea7abe8376652905378e18a3272
KAAAsS <admin@kaaass.net>
2020-03-06 18:25:31 +0800

    完成区块链管理

--- src/blockchain/Blockchain.cpp
+++ src/blockchain/Blockchain.cpp
@@ -1,1 +1,38 @@
 #include "Blockchain.h"
+
+UInt Blockchain::create(const std::string &data) {
+    ChainBlock *newBlock = nullptr;
+    UInt32 prevHash;
+    UInt blockId = size();
+
+    // 计算前一块哈希
+    if (size() > 0) {
+        prevHash = get(size() - 1).getBlockHash();
+    } else {
+        prevHash = 0u; // 第一块则为0
+    }
+
+    // 创建块
+    newBlock = new ChainBlock(data, prevHash, blockId);
+    // 加入链
+    GLOBAL_CHAIN.push_back(std::ref(*newBlock));
+
+    return blockId;
+}
+
+const ChainBlock &Blockchain::get(UInt blockId) {
+    assert(blockId < size());
+    return GLOBAL_CHAIN[blockId];
+}
+
+size_t Blockchain::size() {
+    return GLOBAL_CHAIN.size();
+}
+
+std::vector<std::reference_wrapper<const ChainBlock>> Blockchain::getGlobalChain() {
+    std::vector<std::reference_wrapper<const ChainBlock>> ret(size());
+    for (auto &ref: GLOBAL_CHAIN) {
+        ret.emplace_back(ref.get());
+    }
+    return ret;
+}
--- src/blockchain/Blockchain.h
+++ src/blockchain/Blockchain.h
@@ -2,12 +2,47 @@
 #define NEWS_BLOCKCHAIN_BLOCKCHAIN_H
 
 #include <util/Types.h>
+#include <vector>
+#include <string>
+#include <blockchain/ChainBlock.h>
 
 /**
  * 区块链管理类
  */
 class Blockchain {
 
+    /**
+     * 保存所有区块链的引用
+     */
+    static std::vector<std::reference_wrapper<ChainBlock>> GLOBAL_CHAIN;
+
+public:
+
+    /**
+     * 创建区块至区块链
+     * @param data
+     * @return 该区块的ID
+     */
+    static UInt create(const std::string &data);
+
+    /**
+     * 通过区块ID获得一个区块
+     * @param blockId
+     * @return
+     */
+    static const ChainBlock& get(UInt blockId);
+
+    /**
+     * 当前区块链大小
+     * @return
+     */
+    static size_t size();
+
+    /**
+     * 获得所有区块的引用
+     * @return
+     */
+    static std::vector<std::reference_wrapper<const ChainBlock>> getGlobalChain();
 };
 
 #endif //NEWS_BLOCKCHAIN_BLOCKCHAIN_H
