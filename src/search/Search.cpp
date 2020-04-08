396e201aaa41e18486bffdd0d1515ad0da015517
KAAAsS <admin@kaaass.net>
2020-04-08 13:39:57 +0800

    审计search相关代码

--- src/search/Search.cpp
+++ src/search/Search.cpp
@@ -1,164 +1,145 @@
 #include "Search.h"
+
 std::vector<int> Search::moveLenth;
- std::vector<UInt32> Search::search(std::vector<std::string>& keyWords) {
 
-	 Max max{ 0, -1 }, second{ 0,-1 }, third{ 0,-1 };
-	std::vector<ByteBuffer> key;//chushihua
-	if (keyWords.empty())
-	{
-		std::vector<UInt32> nullId;
-		return nullId;
-	}
+std::vector<UInt32> Search::search(std::vector<std::string> &keyWords) {
 
-	//×Ö·û´®×ª×Ö½ÚÁ÷
-	for (auto const& value : keyWords)
-	{
-		key.push_back(ByteBuffer::str(value));
-	}
-	int n = 0;
-	auto temp = Blockchain::getGlobalChain();//·µ»ØÇø¿éÁ´µÄÒıÓÃ
-	for (auto& blockRef : temp)
-	{
-		auto realBlock = blockRef.get();
-		if (realBlock.hasKeyword(keyWords))
-		{
-			std::vector<ByteBuffer> ret = realBlock.getAllDecomposedDataBlock();//µÃµ½ËùÓĞÊı¾İ¿éµÄ½âÑ¹Êı¾İ
-			int total = 0, num = 0;
-			for (auto& k : key)
-			{
+    Max max{0, -1}, second{0, -1}, third{0, -1};
+    std::vector<ByteBuffer> key;//chushihua
+    if (keyWords.empty()) {
+        std::vector<UInt32> nullId;
+        return nullId;
+    }
 
-				num = Sunday(k, ret);
-				if (num == 0)
-				{
-					n = 0;
-					break;
-				}
-				else
-				{
-					total += num;
-					n = 1;
-				}
-			}
-			if (n)
-			{
-				switch (location(total,max.count,second.count,third.count))
-				{
-					case 1:
-						{
-							third = second;
-							second = max;
-							max.count = total;
-							max.id = realBlock.getHeader().blockId;
-						}break;
-					case 2:
-						{
-							third = second;
-							second.count = total;
-							second.id = realBlock.getHeader().blockId;
-						}break;
-					case 3:
-						{
-							third.count = total;
-							third.id= realBlock.getHeader().blockId;
-						}break;
-				default:
-					break;
-				}
-				
-			}
-		}
-	}
-	std::vector<UInt32> result;
-	if (max.count)
-		result.push_back(max.id);
-	if (second.count)
-		result.push_back(second.id);
-	if (third.count)
-		result.push_back(third.id);
-	return result;
+    //å­—ç¬¦ä¸²è½¬å­—èŠ‚æµ
+    for (auto const &value : keyWords) {
+        key.push_back(ByteBuffer::str(value));
+    }
+    int n = 0;
+    auto temp = Blockchain::getGlobalChain();//è¿”å›åŒºå—é“¾çš„å¼•ç”¨
+    for (auto &blockRef : temp) {
+        auto realBlock = blockRef.get();
+        if (realBlock.hasKeyword(keyWords)) {
+            std::vector<ByteBuffer> ret = realBlock.getAllDecomposedDataBlock();//å¾—åˆ°æ‰€æœ‰æ•°æ®å—çš„è§£å‹æ•°æ®
+            int total = 0, num = 0;
+            for (auto &k : key) {
+
+                num = Sunday(k, ret);
+                if (num == 0) {
+                    n = 0;
+                    break;
+                } else {
+                    total += num;
+                    n = 1;
+                }
+            }
+            if (n) {
+                switch (location(total, max.count, second.count, third.count)) {
+                    case 1: {
+                        third = second;
+                        second = max;
+                        max.count = total;
+                        max.id = realBlock.getHeader().blockId;
+                    }
+                        break;
+                    case 2: {
+                        third = second;
+                        second.count = total;
+                        second.id = realBlock.getHeader().blockId;
+                    }
+                        break;
+                    case 3: {
+                        third.count = total;
+                        third.id = realBlock.getHeader().blockId;
+                    }
+                        break;
+                    default:
+                        break;
+                }
+
+            }
+        }
+    }
+    std::vector<UInt32> result;
+    if (max.count)
+        result.push_back(max.id);
+    if (second.count)
+        result.push_back(second.id);
+    if (third.count)
+        result.push_back(third.id);
+    return result;
 }
 
-int Search::Sunday(ByteBuffer& key, std::vector<ByteBuffer>& block)
-{
-	getMovelenth(key);
-	ByteBuffer newByteBuffer;
-	for (auto i = 0u; i < block.size(); i++)
-	{
-		if (i != 0u)
-			newByteBuffer.push_back(' ');
-		newByteBuffer = newByteBuffer.operator+(block.operator[](i));//Æ´½Ó»º³åÇø
-	}
-	auto k = key.data();
-	auto b = newByteBuffer.data();
-	int klen = key.size();
-	int bufflen = newByteBuffer.size();
-	bool flag = false;
-	int i = 0, count = 0, j;
-	while (i <bufflen)
-	{
-		 j = 0;
+int Search::Sunday(ByteBuffer &key, std::vector<ByteBuffer> &block) {
+    getMovelenth(key);
+    ByteBuffer newByteBuffer;
+    for (auto i = 0u; i < block.size(); i++) {
+        if (i != 0u)
+            newByteBuffer.push_back(' ');
+        newByteBuffer = newByteBuffer.operator+(block.operator[](i));//æ‹¼æ¥ç¼“å†²åŒº
+    }
+    auto k = key.data();
+    auto b = newByteBuffer.data();
+    int klen = key.size();
+    int bufflen = newByteBuffer.size();
+    bool flag = false;
+    int i = 0, count = 0, j;
+    while (i < bufflen) {
+        j = 0;
 
-		 for (; j < klen && i + j < bufflen && b[i + j] == k[j]; ++j);
-		
-		 if (j >= klen&&(i+j>=bufflen||!isChar(b[i+j])))
-		 {
-			 count++;
-			 flag = true;
-		 }
-		 if (i + klen >= bufflen)
-			 return count;
-		 
-		 if (flag)
-		 {
-			 
-			 for (++j; i + j  < bufflen && !isChar(b[i+j]); j++);
-			 i += j;
-			 flag == false;
-		 }
-			 
-		 else
-		 {	
-			 for (j; i + j < bufflen && isChar(b[i + j]); j++);
-			 for (; i + j < bufflen && !isChar(b[i + j]); j++);
-			 if (i + j >= bufflen)
-				 return count;
-			 else
-				 //i += moveLenth[b[i + j]];
-				 i += j;
+        for (; j < klen && i + j < bufflen && b[i + j] == k[j]; ++j);
 
-		 }
+        if (j >= klen && (i + j >= bufflen || !isChar(b[i + j]))) {
+            count++;
+            flag = true;
+        }
+        if (i + klen >= bufflen)
+            return count;
 
-	}
+        if (flag) {
+
+            for (++j; i + j < bufflen && !isChar(b[i + j]); j++);
+            i += j;
+            flag == false;
+        } else {
+            for (j; i + j < bufflen && isChar(b[i + j]); j++);
+            for (; i + j < bufflen && !isChar(b[i + j]); j++);
+            if (i + j >= bufflen)
+                return count;
+            else
+                //i += moveLenth[b[i + j]];
+                i += j;
+
+        }
+
+    }
 
 }
 
-void Search::getMovelenth( ByteBuffer &key)
-{
-	int klen = key.size();
+void Search::getMovelenth(ByteBuffer &key) {
+    int klen = key.size();
 
-	for (auto i = 0; i < MAXNUM; i++)
-		moveLenth.push_back(klen + 1);
-	for (auto i = 0; i < klen; i++)
-		moveLenth[key.operator[](i)] = klen - i;
+    for (auto i = 0; i < MAXNUM; i++)
+        moveLenth.push_back(klen + 1);
+    for (auto i = 0; i < klen; i++)
+        moveLenth[key.operator[](i)] = klen - i;
 }
 
-int location(int a, int b, int c, int d)
-{
-	if (a > b)
-		return 1;
-	if (a > c)
-		return 2;
-	if (a > d)
-		return 3;
-	return 0;
+int location(int a, int b, int c, int d) {
+    if (a > b)
+        return 1;
+    if (a > c)
+        return 2;
+    if (a > d)
+        return 3;
+    return 0;
 
 }
 
-bool isChar(Byte& b)
-{
-	
-	if ((b <= 'Z' && b >= 'A') || (b<='z' && b >='a') || (b<='9' && b >= '0'))
-		return true;
-	else
-		return false;
+bool isChar(Byte &b) {
+
+    if ((b <= 'Z' && b >= 'A') || (b <= 'z' && b >= 'a') || (b <= '9' && b >= '0'))
+        return true;
+    else
+        return false;
 }
--- test/search/TestSearch.cpp
+++ test/search/TestSearch.cpp
@@ -3,55 +3,54 @@
 #include <search/Search.h>
 
 
-TEST(testSearch,testSunday) {
-	
-	ByteBuffer key;//chushihua
-	key=ByteBuffer::str("abc");
-	
-	std::vector<ByteBuffer> block;
-	ByteBuffer a = ByteBuffer::str("abc");
-	ByteBuffer b = ByteBuffer::str("  cdfabcw abc");
-	block.push_back(a);
-	block.push_back(b);
-	ByteBuffer* c = &block[0];
-	int num=Search::Sunday(key, block);//Ó¦¸ÃÎª2
-	 // Á½¸öµÄ½á¹ûÓ¦ÏàÍ¬
-	ASSERT_EQ((UInt)2, (UInt)num); //
-	ASSERT_EQ((UInt)2, (UInt)block.size());
-	//ASSERT_EQ((UInt)c, (UInt)block.data());
+TEST(TestSearch, testSunday) {
+
+    ByteBuffer key;//chushihua
+    key = ByteBuffer::str("abc");
+
+    std::vector<ByteBuffer> block;
+    ByteBuffer a = ByteBuffer::str("abc");
+    ByteBuffer b = ByteBuffer::str("  cdfabcw abc");
+    block.push_back(a);
+    block.push_back(b);
+    ByteBuffer *c = &block[0];
+    int num = Search::Sunday(key, block);//åº”è¯¥ä¸º2
+    // ä¸¤ä¸ªçš„ç»“æœåº”ç›¸åŒ
+    ASSERT_EQ((UInt) 2, (UInt) num); //
+    ASSERT_EQ((UInt) 2, (UInt) block.size());
+    //ASSERT_EQ((UInt)c, (UInt)block.data());
 }
 
-TEST(testSearch, testgetMovelenth)
-{
-	
-	std::vector<int> move;
-	ByteBuffer key({ 'a', 'b', 'c' });
-	int klen = key.size();
+TEST(TestSearch, testgetMovelenth) {
 
-	for (auto i = 0; i < MAXNUM; i++)
-		move.push_back(klen + 1);
-	for (auto i = 0; i < klen; i++)
-		move[key.operator[](i)] = klen - i;
-	ASSERT_EQ(move['a'], 3);
-	ASSERT_EQ(move['b'], 2);
-	ASSERT_EQ(move[key.operator[](2)], 1);
-	
+    std::vector<int> move;
+    ByteBuffer key({'a', 'b', 'c'});
+    int klen = key.size();
+
+    for (auto i = 0; i < MAXNUM; i++)
+        move.push_back(klen + 1);
+    for (auto i = 0; i < klen; i++)
+        move[key.operator[](i)] = klen - i;
+    ASSERT_EQ(move['a'], 3);
+    ASSERT_EQ(move['b'], 2);
+    ASSERT_EQ(move[key.operator[](2)], 1);
+
 }
 
-TEST(testSearch, testsearch)
-{
-	
-	Blockchain::create("abc\nabcc\nccc\nabcc abcc\nac \nccc");
-	Blockchain::create("ccc abcb\nabc\nac");
-	Blockchain::create("cac\nabcabc\nac");
-	ASSERT_EQ(3u, Blockchain::size());
-	std::vector<std::string> key;
-	key.push_back("abc");
-	key.push_back("ac");
-	key.push_back("ccc");
-	ASSERT_EQ( (UInt32)2, Search::search(key).size());
-	
+TEST(TestSearch, testsearch) {
+    Blockchain::clear();
+
+    Blockchain::create("abc\nabcc\nccc\nabcc abcc\nac \nccc");
+    Blockchain::create("ccc abcb\nabc\nac");
+    Blockchain::create("cac\nabcabc\nac");
+    ASSERT_EQ(3u, Blockchain::size());
+    std::vector<std::string> key;
+    key.push_back("abc");
+    key.push_back("ac");
+    key.push_back("ccc");
+    auto ret = Search::search(key);
+    ASSERT_EQ((UInt32) 2, ret.size());
+    ASSERT_EQ(std::vector<UInt32>({0, 1}), ret);
 }
 
-
-// ×ÔĞĞ·¢»Ó
+// è‡ªè¡Œå‘æŒ¥
