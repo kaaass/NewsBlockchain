d79d2e67be0574d25ab9d76813a731db10ce64e1
KAAAsS <admin@kaaass.net>
2020-04-08 13:47:03 +0800

    审计huffman相关代码

--- test/huffman/TestHuffman.cpp
+++ test/huffman/TestHuffman.cpp
@@ -3,9 +3,9 @@
 #include <huffman/Huffman.h>
 
 TEST(TestHuffman, testRun) {
-	ByteBuffer buffer;
-	std::vector<ByteBuffer> buffer_vector;
-	std::string str = "Anti Tracks is a complete solution \
+    ByteBuffer buffer;
+    std::vector<ByteBuffer> buffer_vector;
+    std::string str = "Anti Tracks is a complete solution \
 to protect your privacy and enhance your PC performance. \
 With a simple click Anti Tracks securely erase your internet \
 tracks, computer activities and programs history information \
@@ -19,17 +19,17 @@ or at Windows stat-up/ shutdown.To ensure maximum privacy protection \
 Anti Tracks implements the US Department of Defense DOD 5220.22-M, \
 Gutmann and NSA secure erasing methods, making any erased files \
 unrecoverable even when using advanced recovery tools.";
-	for (std::string::iterator iter = str.begin(); iter != str.end(); iter++) {
-		buffer.push_back(*iter);
-	}
-	buffer_vector.push_back(buffer);
-	Huffman::Result result = Huffman::compress(buffer_vector);
-	ByteBuffer ans = Huffman::decompress(result.dictionary, result.data.at(0));
-	std::string str2;
-	for (size_t i = 0; i < buffer.size(); i++) {
-		str2.push_back(ans[i]);
-	}
-	ASSERT_EQ((UInt)str.compare(str2), (UInt)0);//加密解密后得到的字符串相等
+    for (std::string::iterator iter = str.begin(); iter != str.end(); iter++) {
+        buffer.push_back(*iter);
+    }
+    buffer_vector.push_back(buffer);
+    Huffman::Result result = Huffman::compress(buffer_vector);
+    ByteBuffer ans = Huffman::decompress(result.dictionary, result.data.at(0));
+    std::string str2;
+    for (size_t i = 0; i < buffer.size(); i++) {
+        str2.push_back(ans[i]);
+    }
+    ASSERT_EQ((UInt) str.compare(str2), (UInt) 0);//加密解密后得到的字符串相等
 }
 
 // 自行发挥
